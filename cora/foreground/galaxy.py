93db472173e08a5a1cbc98f5adae2eacd3e7c0a4
Richard Shaw <jrs65@cita.utoronto.ca>
2014-03-17 14:16:39 -0400

    New galactic pol model.

--- cora/foreground/galaxy.py
+++ cora/foreground/galaxy.py
@@ -172,7 +172,9 @@ class ConstrainedGalaxy(maps.Sky3d):
         ## Get the smooth, large scale emission from Haslam+spectralmap
         fgsmooth = haslam[np.newaxis, :] * ((efreq / 408.0)[:, np.newaxis]**sc)
 
-        fg2 = (fgsmooth + fgt)[2:]
+        tanh_lin = lambda x: np.where(x < 0, np.tanh(x), x)
+
+        fg2 = (fgsmooth * (1.0 + tanh_lin(fgt / fgsmooth)))[2:]
 
         ## Co-ordinate transform if required
         if celestial:
@@ -203,12 +205,8 @@ class ConstrainedGalaxy(maps.Sky3d):
         # Load and smooth the Faraday emission
         sigma_phi = healpy.ud_grade(healpy.smoothing(np.abs(self._faraday), fwhm=np.radians(10.0)), self.nside)
 
-        # Get the Haslam map as a base for the unpolarised emission
-        haslam = healpy.smoothing(healpy.ud_grade(self._haslam, self.nside), fwhm=np.radians(3.0))
-
         # Create a map of the correlation length in phi
-        xiphi = 3.0
-        xiphimap = np.minimum(sigma_phi / 20.0, xiphi)
+        xiphi = 1.0
 
         lmax = 3*self.nside - 1
         la = np.arange(lmax+1)
@@ -218,117 +216,94 @@ class ConstrainedGalaxy(maps.Sky3d):
             l[np.where(l == 0)] = 1.0e16
             return (l / 100.0)**-2.8
 
-        c6 = 3e2 # Speed of light in million m/s
-        xf = 1.5 # Factor to exand the region in lambda^2 by.
-
-        ## Calculate the range in lambda^2 to generate
-        l2l = (c6 / self.nu_upper)**2
-        l2h = (c6 / self.nu_lower)**2
-
-        l2a = 0.5 * (l2h + l2l)
-        l2d = 0.5 * (l2h - l2l)
-
-        # Bounds and number of points in lambda^2
-        l2l = l2a - xf * l2d
-        l2h = l2a + xf * l2d
-        nl2 = int(xf * self.nu_num)
-
-        l2 = np.linspace(l2l, l2h, nl2) # Grid in lambda^2 to use
+        dphi = 1.0
+        maxphi = 500.0
+        nphi = 2 * int(maxphi / dphi)
+        phifreq = np.fft.fftfreq(nphi, d=(1.0 / (dphi * nphi)))
 
         ## Make a realisation of c(n, l2)
 
-        # Generate random numbers and weight by powerspectrum
-        w = (np.random.standard_normal((nl2, lmax+1, 2*lmax+1)) + 1.0J * np.random.standard_normal((nl2, lmax+1, 2*lmax+1))) / 2**0.5
+        print "Generating random field."
+        w = (np.random.standard_normal((nphi, lmax+1, 2*lmax+1)) + 1.0J * np.random.standard_normal((nphi, lmax+1, 2*lmax+1))) / 2**0.5
         w *= angular(la[np.newaxis, :, np.newaxis])**0.5
 
-        # Transform from spherical harmonics to maps
-        map1 = np.zeros((nl2, 12*self.nside**2), dtype=np.complex128)
-        print "Making maps"
-        for i in range(nl2):
-            map1[i] = hputil.sphtrans_inv_complex(w[i], self.nside)
+        map1 = np.zeros((12*self.nside**2, nphi), dtype=np.complex128)
+        print "SHTing to maps"
+        for i in range(nphi):
+            map1[:, i] = hputil.sphtrans_inv_complex(w[i], self.nside)
 
-        # Weight frequencies for the internal Faraday depolarisation
-        map1 *= np.exp(-2 * (xiphimap[np.newaxis, :] * l2[:, np.newaxis])**2) / 100.0
-
+        print "FFTing to phi field"
+        map2 = np.fft.fft(map1, axis=1)
 
         if not debug:
-            del w
- 
-        # Transform into phispace
-        map2 = np.fft.fft(map1, axis=0)
+            del map1, w
 
+        pcfreq = np.fft.fftfreq(nphi, d=dphi)
 
-        # Calculate the phi samples
-        phifreq = np.pi * np.fft.fftfreq(nl2, d=(l2[1] - l2[0]))
+        map2 *= np.exp(-2 * (np.pi * xiphi * pcfreq[np.newaxis, :])**2)
+
+        map3 = np.fft.ifft(map2, axis=1)
+        map3 /= (2.0 * map3.std())
 
         if not debug:
-            del map1
+            del map2
 
-        # Create weights for smoothing the emission (corresponding to the
-        # Gaussian region of emission).
-        w = np.exp(-0.25 * (phifreq[:, np.newaxis] / sigma_phi[np.newaxis, :])**2)
+        w = np.exp(-0.25 * (phifreq[np.newaxis, :] / sigma_phi[:, np.newaxis])**2)
 
         # Calculate the normalisation explicitly (required when Faraday depth
         # is small, as grid is too large).
-        w /= w.sum(axis=0)[np.newaxis, :]
+        w /= w.sum(axis=1)[:, np.newaxis]
 
-        # When the spacing between phi samples is too large we don't get the
-        # decorrelation from the independent regions correct.
-        xiphimap = np.maximum(xiphimap, np.pi / (l2h - l2l))
-        xiphimap = np.minimum(xiphimap, sigma_phi)
-        w *= 0.2 * (sigma_phi / xiphimap)**0.5 * (10.0 / haslam)**0.5
-
-        # Additional weighting to account for finite frequency bin width
-        # (assume channels are gaussian). Approximate by performing in
-        # lambda^2 not nu.
-        dnu_nu = np.diff(self.nu_pixels).mean() / self.nu_pixels.mean()
-
-        # Calculate the spacing lambda^2
-        dl2 = 2*l2.mean() * dnu_nu / (8*np.log(2.0))**0.5
-        w *= np.exp(-2.0 * dl2**2 * phifreq**2)[:, np.newaxis]
-
-        # Weight map, and transform back into lambda^2
-        map3 = np.fft.ifft(map2 * w, axis=0)
+        print "Applying phi weighting"
+        map3 *= w
 
         if not debug:
-            del map2, w
+            del w
 
-        # Array to hold frequency maps in.
-        map4 = np.zeros((self.nu_num, 4, 12*self.nside**2), dtype=np.float64)
+        def ptrans(phi, freq, dfreq):
 
-        ## Interpolate lambda^2 sampling into regular frequency grid.
-        ## Use a piecewise linear method
-        for i in range(self.nu_num):
+            dx = dfreq / freq
 
-            # Find the lambda^2 for the current frequency
-            l2i = (c6 / self.nu_pixels[i])**2
+            alpha = 2.0 * phi * 3e2**2 / freq**2
 
-            # Find the bounding array indices in the lambda^2 array
-            ih = np.searchsorted(l2, l2i)
-            il = ih - 1
+            return (np.exp(1.0J * alpha) * np.sinc(alpha * dx / np.pi))
 
-            # Calculate the interpolating coefficient
-            alpha = (l2i - l2[il]) / (l2[ih] - l2[il])
+        fa = self.nu_pixels
+        df = np.median(np.diff(fa))
 
-            # Interpolate each map at the same time.
-            mapint = map3[ih] * alpha + (1.0 - alpha) * map3[il]
+        pta = ptrans(phifreq[:, np.newaxis], fa[np.newaxis, :], df) / dphi
 
-            # Separate real and imaginary parts into polarised matrix
-            map4[i, 1] = mapint.real
-            map4[i, 2] = mapint.imag
+        print "Transforming to freq"
+        map4 = np.dot(map3, pta)
 
         if not debug:
             del map3
 
-        # Unflatten the intensity by multiplying by the unpolarised realisation
-        map4[:, 0] = self.getsky(celestial=False)
-        map4[:, 1:3] *= map4[:, 0, np.newaxis, :]
+        print "Rescaling freq"
+        map4a = np.abs(map4)
+        map4 = map4 * np.tanh(map4a) / map4a
 
+        del map4a
+
+        map5 = np.zeros((self.nu_num, 4, 12 * self.nside**2), dtype=np.float64)
+
+
+        print "Scaling by T"
+        # Unflatten the intensity by multiplying by the unpolarised realisation
+        map5[:, 0] = self.getsky(celestial=False)
+        map5[:, 1] = map4.real.T
+        map5[:, 2] = map4.imag.T
+        map5[:, 1:3] *= map5[:, 0, np.newaxis, :]
+
+        if not debug:
+            del map4
+
+        print "Rotating"
         # Rotate to celestial co-ordinates if required.
         if celestial:
-            map4 = hputil.coord_g2c(map4)
+            map5 = hputil.coord_g2c(map5)
 
         if debug:
-            return map4, map1, map2, map3, w, sigma_phi
+            return map1, map2, map3, map4, w, sigma_phi
         else:
-            return map4
+            return map5
