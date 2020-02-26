596b45de44ae55ddc8febfd27c028eb417f16bd9
Richard Shaw <richard@phas.ubc.ca>
2020-02-25 17:07:06 -0800

    feat(versioneer): add versioneer for improved version naming

--- cora/__init__.py
+++ cora/__init__.py
@@ -5,4 +5,7 @@ from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
 
 # === End Python 2/3 compatibility
 
-__version__ = "2.1.1"
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
