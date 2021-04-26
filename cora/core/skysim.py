cfff3f1988c10d56ca57379a0d260c595d1bf6b9
Simon Foreman <sforeman@cita.utoronto.ca>
2021-04-25 22:04:49 -0700

    fix(clarray): fix bug if b_second_func not specified

--- cora/core/skysim.py
+++ cora/core/skysim.py
@@ -39,6 +39,9 @@ def clarray(aps, lmax, zarray, zromb=3, zwidth=None, b_second_func=None):
         Array of the C_l(z,z') values.
     """
 
+    if b_second_func is None:
+        b_second_func = lambda z : np.ones_like(z)
+
     if zromb == 0:
         return aps(
             np.arange(lmax + 1)[:, np.newaxis, np.newaxis],
