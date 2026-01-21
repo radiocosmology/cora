6761e5f59ec6a9644db3a04aaff7dd6157f0b2d1
Liam Gray <lgray@phas.ubc.ca>
2026-01-21 11:00:21 -0800

    feat(build): add a more robust check for compiler openMP support

--- setup.py
+++ setup.py
@@ -5,22 +5,61 @@ required to build cython extensions.
 """
 
 import os
-import re
+import tempfile
 import sysconfig
+import subprocess
 
 import numpy
 from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
 
-# Decide whether to use OpenMP or not
-if ("CORA_NO_OPENMP" in os.environ) or (
-    re.search("gcc", sysconfig.get_config_var("CC")) is None
-):
-    print("Not using OpenMP")
-    omp_args = ["-std=c99"]
-else:
-    omp_args = ["-std=c99", "-fopenmp"]
+# Subset of `-ffast-math` compiler flags which should
+# preserve IEEE compliance
+FAST_MATH_ARGS = ["-O3", "-fno-math-errno", "-fno-trapping-math", "-march=native"]
+
+
+def _compiler_supports_openmp():
+    cc = os.environ.get("CC", sysconfig.get_config_var("CC"))
+    if cc is None:
+        return False
+
+    test_code = r"""
+    #include <omp.h>
+    int main(void) {
+        return omp_get_max_threads();
+    }
+    """
+
+    with tempfile.TemporaryDirectory() as d:
+        src = os.path.join(d, "test.c")
+        exe = os.path.join(d, "test")
+        with open(src, "w") as f:
+            f.write(test_code)
+
+        cmd = [cc, src, "-fopenmp", "-o", exe]
+        try:
+            subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except Exception:  # noqa: BLE001
+            return False
+
+    return True
+
+
+OMP_ARGS = ["-std=c99"]
+
+if not os.environ.get("CORA_NO_OPENMP"):
+    if _compiler_supports_openmp():
+        OMP_ARGS.append("-fopenmp")
+    else:
+        cc = os.environ.get("CC", sysconfig.get_config_var("CC"))
+        print(
+            f"Compiler `{cc}` does not support OpenMP. "
+            "If an OpenMP-supporting compiler is available, "
+            "add it to your PATH or use `CC=<compiler> pip install ...` "
+            "Alternatively, to suppress this warning, set the environment "
+            "variable CORA_NO_OPENMP to any truth-like value."
+        )
 
 extensions = [
     # Cubic spline extension
@@ -28,24 +67,24 @@ extensions = [
         "cora.util.cubicspline",
         ["cora/util/cubicspline.pyx"],
         include_dirs=[numpy.get_include()],
-        extra_compile_args=omp_args,
-        extra_link_args=omp_args,
+        extra_compile_args=[*FAST_MATH_ARGS, *OMP_ARGS],
+        extra_link_args=[*FAST_MATH_ARGS, *OMP_ARGS],
     ),
     # Bi-linear map extension
     Extension(
         "cora.util.bilinearmap",
         ["cora/util/bilinearmap.pyx"],
         include_dirs=[numpy.get_include()],
-        extra_compile_args=omp_args,
-        extra_link_args=omp_args,
+        extra_compile_args=[*FAST_MATH_ARGS, *OMP_ARGS],
+        extra_link_args=[*FAST_MATH_ARGS, *OMP_ARGS],
     ),
     # particle-mesh gridding extension
     Extension(
         "cora.util.pmesh",
         ["cora/util/pmesh.pyx"],
         include_dirs=[numpy.get_include()],
-        extra_compile_args=omp_args,
-        extra_link_args=omp_args,
+        extra_compile_args=[*FAST_MATH_ARGS, *OMP_ARGS],
+        extra_link_args=[*FAST_MATH_ARGS, *OMP_ARGS],
     ),
 ]
 
