e9ca8774a836bbc6bcd45c38292edb34791954dc
Liam Gray <lgray@phas.ubc.ca>
2026-02-06 12:46:17 -0800

    feat(build): include proper detection for apple metal CPUs

--- setup.py
+++ setup.py
@@ -5,28 +5,52 @@ required to build cython extensions.
 """
 
 import os
-import tempfile
-import sysconfig
+import platform
+import re
 import subprocess
+import sysconfig
+import tempfile
 
 import numpy
 from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
 
-USE_LTO = True
-
 # Subset of `-ffast-math` compiler flags which should
 # preserve IEEE compliance
 FAST_MATH_ARGS = ["-O3", "-fno-math-errno", "-fno-trapping-math"]
 # Flags required by the compiler
-COMPILE_FLAGS = ["-std=c99", "-march=native", *FAST_MATH_ARGS]
-LINK_FLAGS = []
+COMPILE_FLAGS = ["-flto", *FAST_MATH_ARGS]
 
-if USE_LTO:
-    COMPILE_FLAGS.append("-flto")
-    # Flags required by the linker
-    LINK_FLAGS.extend(("-flto", *FAST_MATH_ARGS))
+
+def get_mcpu_flag():
+    """Try to figure out the relevant cpu flags."""
+    system = platform.system().lower()
+
+    if system != "darwin":
+        # Making no assumptions about other systems
+        return "-march=native" if system == "linux" else ""
+
+    try:
+        # Get CPU model: "Apple M3 Pro", etc.
+        cpu_brand = (
+            subprocess.check_output(["sysctl", "-n", "machdep.cpu.brand_string"])
+            .decode()
+            .strip()
+            .lower()
+        )
+    except:  # noqa: E722
+        return ""
+
+    # See if we can match against specifc Metal generations
+    match = re.match(r"apple m\d", cpu_brand)
+
+    if match:
+        cpu = match[0].lower().strip().replace(" ", "-")
+
+        return f"-mcpu={cpu}"
+
+    return "-mcpu=apple-m1"
 
 
 def _compiler_supports_openmp():
@@ -61,7 +85,6 @@ if not os.environ.get("CORA_NO_OPENMP"):
     if _compiler_supports_openmp():
         # OpenMP flags are required by both the compiler and the linker
         COMPILE_FLAGS.append("-fopenmp")
-        LINK_FLAGS.append("-fopenmp")
     else:
         cc = os.environ.get("CC", sysconfig.get_config_var("CC"))
         print(
@@ -72,6 +95,12 @@ if not os.environ.get("CORA_NO_OPENMP"):
             "variable CORA_NO_OPENMP to any truth-like value."
         )
 
+if "-fopenmp" not in COMPILE_FLAGS and platform.system().lower() == "darwin":
+    # Use Apple Accelerate
+    COMPILE_FLAGS.extend(("-framework", "Accelerate"))
+
+COMPILE_FLAGS.append(get_mcpu_flag())
+
 extensions = [
     # Cubic spline extension
     Extension(
@@ -79,7 +108,7 @@ extensions = [
         ["cora/util/cubicspline.pyx"],
         include_dirs=[numpy.get_include()],
         extra_compile_args=COMPILE_FLAGS,
-        extra_link_args=LINK_FLAGS,
+        extra_link_args=COMPILE_FLAGS,
     ),
     # Bi-linear map extension
     Extension(
@@ -87,7 +116,7 @@ extensions = [
         ["cora/util/bilinearmap.pyx"],
         include_dirs=[numpy.get_include()],
         extra_compile_args=COMPILE_FLAGS,
-        extra_link_args=LINK_FLAGS,
+        extra_link_args=COMPILE_FLAGS,
     ),
     # particle-mesh gridding extension
     Extension(
@@ -95,7 +124,7 @@ extensions = [
         ["cora/util/pmesh.pyx"],
         include_dirs=[numpy.get_include()],
         extra_compile_args=COMPILE_FLAGS,
-        extra_link_args=LINK_FLAGS,
+        extra_link_args=COMPILE_FLAGS,
     ),
 ]
 
